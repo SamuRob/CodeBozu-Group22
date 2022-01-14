b976c0bac6b104aa2a21e57c1205cb51985c190a
SamuRob <87442361+SamuRob@users.noreply.github.com>
2022-01-14 23:08:44 +0300

    Update eastwestcoast.py

--- Biodata/eastwestcoast.py
+++ Biodata/eastwestcoast.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import csv
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
+import matplotlib.pyplot as plt
 
 east_coast_states=["Maine", "New Hampshire", "Massachusetts", "Rhode Island", "Connecticut", "New York", "New Jersey", "Delaware", "Maryland", "Virginia", "North Carolina", "South Carolina", "Georgia", "Florida"]
 west_coast_states=["California","Oregon","Washington","Alaska","Hawaii"]
@@ -12,18 +13,28 @@ file = open(folder + "/" + 'Deliverable 1.csv')
 csvreader = csv.reader(file)
 birth_area=[]
 all_the_people = []
-people_from_east_coast=0
-people_from_west_coast=0
+east_coast=0
+west_coast=0
 for row in csvreader:
     all_the_people.append(row)
 for birth in all_the_people:
     for cities in east_coast_states:
         if cities in birth[2]:
-            people_from_east_coast+=1
+            east_coast+=1
 
 for birth in all_the_people:
     for cities in west_coast_states:
         if cities in birth[2]:
-            people_from_west_coast+=1
-print(people_from_east_coast)
-print(people_from_west_coast)
+            west_coast+=1
+
+left = [1,2]
+height = [west_coast , east_coast]
+tick_label = ['West Coast','East Coast']
+plt.bar(left,height,tick_label = tick_label,
+        width = 0.8,color = ['red','green'])
+
+plt.xlabel('Coasts')
+plt.ylabel('Presidents and Vice Presidents')
+plt.title('Distribution of US Vice and Presidents on West vs East Coast')
+
+plt.show()
